e636a998276f70e6ccee0cb6950f05c18cda5042
plasmatech8 <com0004@gmail.com>
2020-05-13 14:43:08 +1000

    Updated lint + add product  migrations

--- src/products/models.py
+++ src/products/models.py
@@ -5,11 +5,11 @@ from django.urls import reverse
 
 
 class Product(models.Model):
-    title       = models.CharField(max_length=120)
+    title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    price       = models.DecimalField(decimal_places=2, max_digits=1000)
-    summary     = models.TextField(blank=False, null=False)
-    featured    = models.BooleanField(default=False)
+    price = models.DecimalField(decimal_places=2, max_digits=1000)
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse("products:product-detail", kwargs={"p_id": self.id})
